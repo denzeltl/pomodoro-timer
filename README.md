9ac157a1cd1e83d697ea3b19470454a0d8700cde
Denzel <denzel706@yahoo.com.ph>
2019-09-16 00:29:01 +0800

    finish main js

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# pomodoro
+# Pomodoro Timer
+
+A simple Pomodoro Timer with extra features (Reminds to drink water, rest eyes, sit / stand)
+
+View [here](PROJECT URL)
+
+**Built with:** _HTML, CSS, SASS, JavaScript_
+
+**Completed on:** _mm/dd/yyyy_
+
+_Sounds from: [https://github.com/jonobr1/Neuronal-Synchrony]_
+
+---
+
+## Things Learned:
+
+-   LEARNING
---
+++ main.js
@@ -0,0 +1,59 @@
+const app = () => {
+    const redCircle = document.querySelector("#red-circle path");
+    const sessions = document.querySelector("#sessions");
+    const timer = document.querySelector("#timer");
+    const command = document.querySelector("#command");
+    const play = document.querySelector("#play");
+    const pause = document.querySelector("#pause");
+    let minutesDuration = 60;
+    let sessionCounter = 1;
+    let redCircleOutline = document.querySelector("#red-circle path").getTotalLength();
+
+    // Add event for play button
+    play.addEventListener("click", e => {
+        play.classList.add("hidden");
+        pause.classList.remove("hidden");
+
+        // Play button function
+        const runCountdown = () => {
+            minutesDuration -= 1;
+            let timerText = `${Math.floor(minutesDuration / 60)}:${Math.floor(minutesDuration % 60)}`;
+            timer.textContent = timerText;
+            sessions.textContent = `Session ${sessionCounter}`;
+            command.textContent = "Focus";
+            redCircleOutline -= redCircleOutline / minutesDuration;
+            redCircle.style.strokeDashoffset = redCircleOutline;
+
+            // if minutesDuration <= 0
+            TODO:BREAK TIMES AND EXTRA FEATURES
+            if (minutesDuration <= 0) {
+            }
+
+            // If minutes < 10
+            if (Math.floor(minutesDuration / 60 < 10)) {
+                timerText = `0${Math.floor(minutesDuration / 60)}:${Math.floor(minutesDuration % 60)}`;
+                timer.textContent = timerText;
+            }
+            // If seconds < 10
+            if (Math.floor(minutesDuration % 60 < 10)) {
+                timerText = `${Math.floor(minutesDuration / 60)}:0${Math.floor(minutesDuration % 60)}`;
+                timer.textContent = timerText;
+            }
+            // If minutes and seconds < 10
+            if (Math.floor(minutesDuration / 60 < 10) && Math.floor(minutesDuration % 60 < 10)) {
+                timerText = `0${Math.floor(minutesDuration / 60)}:0${Math.floor(minutesDuration % 60)}`;
+                timer.textContent = timerText;
+            }
+        };
+        const countdown = setInterval(runCountdown, 1000);
+
+        // Add event for pause button
+        pause.addEventListener("click", e => {
+            pause.classList.add("hidden");
+            play.classList.remove("hidden");
+            clearInterval(countdown);
+        });
+    });
+};
+
+app();
